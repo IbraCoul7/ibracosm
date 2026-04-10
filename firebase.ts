089aabb9fa9e7ceb42da60d3d8f839aef8502db7
Ibrahima Coulibaly <ic6687514@gmail.com>
2026-04-10 10:00:17 +0000

    Configuration Firebase finale et propre

--- firebase.ts
+++ firebase.ts
@@ -1,8 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from './firebase-applet-config.json';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCnGTYNG9RFz9RidicWqttMNTRSWYdr8BA",
+  authDomain: "ibracosm.firebaseapp.com",
+  projectId: "ibracosm",
+  storageBucket: "ibracosm.firebasestorage.app",
+  messagingSenderId: "715850665185",
+  appId: "1:715850665185:web:541fe68bfc8f0ded503ba0"
+};
+
+// Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
+// Exports pour ton application (App.tsx et autres composants)
+export const db = getFirestore(app);
 export const auth = getAuth(app);
